1a27ebb14b7ae435f191eeef2868833aa14ddf75
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2023-12-23 20:42:47 +0300

    refactor: refactor css styles

--- online-bank/src/components/Products/Dropdown/EmptyContainer/EmptyContainer.tsx
+++ online-bank/src/components/Products/Dropdown/EmptyContainer/EmptyContainer.tsx
@@ -5,9 +5,8 @@ import "./style.css";
 export const EmptyContainer = () => {
   return (
     <div className="dropdown__empty-contaner">
-      <Typography text="You don't have a registered product yet" />
-
-      <Typography color="green">
+      <Typography>
+        You don&apos;t have a registered product yet{" "}
         <a className="dropdown__empty-link">More details</a>
       </Typography>
     </div>
